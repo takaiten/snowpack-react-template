255d96e2029c03d9cfa0f108ef11c06b62e07c36
takaiten <lopilopj@gmail.com>
2021-04-09 21:43:13 +0700

    Base setup

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,30 +1,28 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  mount: {
-    public: '/',
-    src: '/dist',
-  },
-  alias: {
-    "~": "./src",
-  },
-  plugins: [
-    '@snowpack/plugin-react-refresh',
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+    mount: {
+        public: '/',
+        src: '/dist',
+    },
+    alias: {
+        '~': './src',
+    },
+    plugins: ['@snowpack/plugin-react-refresh'],
+    routes: [
+        /* Enable an SPA Fallback in development: */
+        // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    ],
+    optimize: {
+        /* Example: Bundle your final build: */
+        // "bundle": true,
+    },
+    packageOptions: {
+        /* ... */
+    },
+    devOptions: {
+        /* ... */
+    },
+    buildOptions: {
+        /* ... */
+    },
 };
--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,8 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
- // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
- // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
- if (import.meta.hot) {
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+if (import.meta.hot) {
     import.meta.hot.accept();
 }
